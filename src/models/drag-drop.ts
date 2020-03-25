ac934ff4df1b0d5a7defe2bbd68b27e070ec4712
Iago <yagomello16@gmail.com>
2020-03-25 17:22:49 -0300

    models: added drag-drop models

---
+++ src/models/drag-drop.ts
@@ -0,0 +1,9 @@
+export interface Draggable {
+  dragStartHandler(evt: DragEvent): void;
+  dragEndHandler(evt: DragEvent): void;
+}
+export interface DragTarget {
+  dragOverHandler(evt: DragEvent): void;
+  dropHandler(evt: DragEvent): void;
+  dragLeaveHandler(evt: DragEvent): void;
+}
