c5915ea335ce3b7651ef6b45bdb0919913cc1120
Iago <yagomello16@gmail.com>
2020-03-25 17:22:10 -0300

    components: added project-item class

---
+++ src/components/project-item.ts
@@ -0,0 +1,42 @@
+import Component from './base-component.js';
+import { Draggable } from '../models/drag-drop.js';
+import { Project } from '../models/project.js';
+import { autobind } from '../decorators/autobind.js';
+
+// ProjectItem Class
+export class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> implements Draggable {
+  private project: Project;
+
+  constructor(parentId: string, project: Project) {
+    super(false, parentId, 'single-project', project.id);
+    this.project = project;
+
+    this.configure();
+    this.renderContent();
+  }
+  
+  get persons() {
+    return this.project.people === 1 ? '1 person' : `${this.project.people} persons`
+  }
+
+  @autobind
+  dragStartHandler(event: DragEvent) {
+    event.dataTransfer!.setData('text/plain', this.project.id);
+    event.dataTransfer!.effectAllowed = 'move';
+  }
+
+  dragEndHandler(_: DragEvent) {
+    console.log('DragEnd');
+  }
+
+  configure() {
+    this.el.addEventListener('dragstart', this.dragStartHandler);
+    this.el.addEventListener('dragend', this.dragEndHandler);
+  }
+
+  renderContent() {
+    this.el.querySelector('h2')!.textContent = this.project.title;
+    this.el.querySelector('h3')!.textContent = this.persons + ' assigned';
+    this.el.querySelector('p')!.textContent = this.project.description;
+  }
+}
