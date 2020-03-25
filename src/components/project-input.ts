35a9657c63e68c41df9f4be5164cabb313f25088
Iago <yagomello16@gmail.com>
2020-03-25 17:21:36 -0300

    components: added project-input class

---
+++ src/components/project-input.ts
@@ -0,0 +1,68 @@
+import Component from './base-component.js';
+import { Validatable, validate } from '../utils/validation.js';
+import { autobind } from '../decorators/autobind.js';
+import { projectState } from '../state/project-state.js';
+
+export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
+  titleInputEl: HTMLInputElement;
+  descriptionInputEl: HTMLInputElement;
+  peopleInputEl: HTMLInputElement;
+
+  constructor() {
+    super(true, 'app', 'project-input', 'user-input');
+    this.titleInputEl = this.el.querySelector('#title') as HTMLInputElement;
+    this.descriptionInputEl = this.el.querySelector( '#description' ) as HTMLInputElement;
+    this.peopleInputEl = this.el.querySelector('#people') as HTMLInputElement;
+    this.configure();
+  }
+
+  configure() {
+    this.el.addEventListener('submit', this.submitForm);
+  }
+
+  renderContent() {}
+
+  private getUserInput(): [string, string, number] | void {
+    const { value: title } = this.titleInputEl;
+    const { value: description } = this.descriptionInputEl;
+    const { value: people } = this.peopleInputEl;
+
+    const titleValidatable: Validatable = {
+      value: title,
+      required: true,
+    };
+    const descriptionValidatable: Validatable = {
+      value: description,
+      required: true,
+      minLength: 5,
+    };
+    const peopleValidatable: Validatable = {
+      value: people,
+      required: true,
+      min: 1,
+      max: 5,
+    };
+
+    if (
+      !validate(titleValidatable) ||
+      !validate(descriptionValidatable) ||
+      !validate(peopleValidatable)
+    ) {
+      alert('Invalid input. Please, try again!');
+      return;
+    }
+
+    return [title, description, +people];
+  }
+
+  @autobind
+  private submitForm(evt: Event) {
+    evt.preventDefault();
+    const userInput = this.getUserInput();
+
+    if (Array.isArray(userInput)) {
+      projectState.addProject(...userInput);
+      this.el.reset();
+    }
+  }
+}
