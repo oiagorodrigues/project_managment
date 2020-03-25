f488e9a72ed52ecd70e86efa10fef6b6406934c4
Iago <yagomello16@gmail.com>
2020-03-25 18:58:20 -0300

    webpack: fixed path bug | added sourcemap | modules rules | resolve extensions

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,25 @@
 const path = require('path');
-function resolve(path) {
-  return path.resolve(__dirname, path);
+function resolve(_path) {
+  return path.resolve(__dirname, _path);
 }
 
 module.exports = {
   entry: './src/app.ts',
   output: {
-    filename: 'bundle.[contenthash].js',
-    path: resolve('dist')
-  }
-}
+    filename: 'bundle.js',
+    path: resolve('dist'),
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+};
